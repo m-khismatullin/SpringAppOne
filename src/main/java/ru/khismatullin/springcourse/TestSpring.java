041d2e807ffa22cf545b56cb9b669819ce8294cd
m-khismatullin <m.khismatullin@yandex.ru>
2023-09-05 20:19:38 +0500

    scope = prototype

--- src/main/java/ru/khismatullin/springcourse/TestSpring.java
+++ src/main/java/ru/khismatullin/springcourse/TestSpring.java
@@ -5,10 +5,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        /*firstMusicPlayer.playMusic();
+        System.out.println(firstMusicPlayer.getName());
+        System.out.println(firstMusicPlayer.getVolume());
+
+        secondMusicPlayer.playMusic();
+        System.out.println(secondMusicPlayer.getName());
+        System.out.println(secondMusicPlayer.getVolume());*/
+
         context.close();
     }
 }
