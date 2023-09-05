16a1ae17a23d755e5d16e576a3577f48b67226eb
m-khismatullin <m.khismatullin@yandex.ru>
2023-09-05 19:43:38 +0500

    вариант 3.3 - внедрение зависимости типа "список"

---
+++ src/main/java/ru/khismatullin/springcourse/FolkMusic.java
@@ -0,0 +1,8 @@
+package ru.khismatullin.springcourse;
+
+public class FolkMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Гори, гори, моя звезда";
+    }
+}
--- src/main/java/ru/khismatullin/springcourse/MusicPlayer.java
+++ src/main/java/ru/khismatullin/springcourse/MusicPlayer.java
@@ -1,22 +1,25 @@
 package ru.khismatullin.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        musicList.stream().forEach(e->System.out.println("Playing \"" + e.getSong() + "\"..."));
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String getName() {
