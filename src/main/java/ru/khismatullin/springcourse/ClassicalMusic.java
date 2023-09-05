e0a484b5c58b0137e8edca5b8258b7f32b5702a3
m-khismatullin <m.khismatullin@yandex.ru>
2023-09-05 17:34:06 +0500

    вариант 1 - внедерение зависимостей через конструктор

---
+++ src/main/java/ru/khismatullin/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.khismatullin.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Phapsody";
+    }
+}
---
+++ src/main/java/ru/khismatullin/springcourse/JazzMusic.java
@@ -0,0 +1,8 @@
+package ru.khismatullin.springcourse;
+
+public class JazzMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Take Five";
+    }
+}
